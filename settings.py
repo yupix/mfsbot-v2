25b6e2ab8185bcbd2f0652eb1f4dc79a1d5a8ede
yupix <yupi0982@outlook.jp>
2020-07-02 23:14:35 +0900

    test: test whether environment variables can be used

--- settings.py
+++ settings.py
@@ -1,10 +1,12 @@
-#botのバージョン(helpコマンド等で使用される)
-bot_version="1.0.0"
-#botの名前(Discordコマンドで使用される)
-bot_name="mfsbot-v2"
-#botのtoken(必須)
-bot_token="ENV['DISCORD_BOT_TOKEN']"
-#botのコマンドにつける!playの!の部分に当たる物
-bot_prefix="yx!"
-#botステータス
-bot_status="テスト稼働中"
+import os
+
+# botのバージョン(helpコマンド等で使用される)
+bot_version = "1.0.0"
+# botの名前(Discordコマンドで使用される)
+bot_name = "mfsbot-v2"
+# botのtoken(必須)
+bot_token = os.environ["DISCORD_BOT_TOKEN"]
+# botのコマンドにつける!playの!の部分に当たる物
+bot_prefix = "yx!"
+# botステータス
+bot_status = "テスト稼働中"
